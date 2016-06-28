b7bdf17dbd00f8827d50d1100bea5f6cf529b321
enekogb <perreko@gmail.com>
2016-06-28 09:35:20 +0200

    Update Renta4_Fondotop_Mejorado.user.js
    Incluido icono euro en traspasos pendientes

--- scripts/Renta4_Fondotop_Mejorado.user.js
+++ scripts/Renta4_Fondotop_Mejorado.user.js
@@ -10,6 +10,8 @@
 
 // Constantes
 var URL_TRASPASOS_PENDIENTES = 'https://www.r4.com/portal?TX=patrimonio&PAG=6&STD=9&HOJA=1';
+var URL_IMG_EURO = 'https://individual.icons-land.com/IconsPreview/POI/PNG/Circled/16x16/Currency_Euro_Circle_Green.png';
+var COD_ISIN_REGEXP = /.*COD_ISIN=([A-Z0-9]+)/i;
 
 // Obtenemos el jquery que usa la pagina
 this.$ = this.jQuery = jQuery.noConflict(true);
@@ -24,6 +26,22 @@ function cargarTraspasosPendientes(nodoTop5) {
     success: function (data) {
       // Introducimos los elementos entre estos dos enlaces delante del TOP 5
       $(data).find('a[name=fondos]').nextUntil('a[name=rentaf]').insertBefore(nodoTop5);
+      
+      // Buscamos los fondos en la tabla de fondos
+      var txtTraspasosPendientes = $(data).text();
+      $(".tablemorning").find('a.msnombrefondo').each(function(index, enlace) {
+        var urlEnlace = $(enlace).attr('href');
+        var m;
+        if ((m = COD_ISIN_REGEXP.exec(urlEnlace)) !== null) {
+          if (m.index === COD_ISIN_REGEXP.lastIndex) {
+            COD_ISIN_REGEXP.lastIndex++;
+          }
+          var isin = m[1];
+          if (txtTraspasosPendientes.indexOf(isin) > -1) {
+            $('<img />').attr('src',URL_IMG_EURO).attr('title', 'Traspaso pendiente').css('margin-right', '3px').insertBefore($(enlace));
+          }
+        }
+      });
     }
   });
 }
